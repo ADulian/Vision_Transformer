83067bad42d190f62e75bb7fed7fde68392f1412
ADulian <duli95@gmail.com>
2023-04-21 15:55:59 +0100

    Basic main

---
+++ main.py
@@ -0,0 +1,44 @@
+import argparse
+
+from torch.utils.data import DataLoader
+from torchvision import datasets,transforms
+
+
+# --------------------------------------------------------------------------------
+def main(args):
+    batch_size = args.batch_size
+    img_size = args.img_size
+
+    # Transforms
+    transform = transforms.Compose([
+        transforms.Resize(img_size),
+        transforms.ToTensor(),
+    ])
+
+    # Data
+    train_set = datasets.MNIST('./data', train=True, download=True, transform=transform)
+    test_set = datasets.MNIST('./data', train=False, download=True, transform=transform)
+
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_set, batch_size=batch_size)
+
+    # Model
+
+    # Training
+
+
+    print("Done")
+
+# --------------------------------------------------------------------------------
+if __name__ == '__main__':
+
+    # Parser
+    parser = argparse.ArgumentParser()
+
+    # Args
+    parser.add_argument('--batch_size', '-bs', type=int, required=False, default=2)
+    parser.add_argument('--img_size', '-is', type=int, required=False, default=32)
+
+    args = parser.parse_args()
+
+    main(args=args)
